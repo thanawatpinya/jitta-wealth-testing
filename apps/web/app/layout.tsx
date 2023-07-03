d15ef56812438aed947fef08aef180663a3b8345
thanawatpinya <thanawatpinya@gmail.com>
2023-07-04 04:32:19 +0700

    optimize the web by adding manifest.json

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -5,6 +5,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+      </head>
       <body>{children}</body>
     </html>
   );
